a71ae7897f36054620461d7a5a1a52e693d660a0
it21288630 <prabashwara.in@gmail.com>
2024-06-15 18:30:58 +0530

    add assets and tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,19 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Slot } from "expo-router";
+import React, { useEffect } from "react";
+import { SplashScreen, Stack } from "expo-router";
 import { NativeWindStyleSheet } from "nativewind";
+import { useFonts } from "expo-font";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
+SplashScreen.preventAutoHideAsync();
+
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) {
+      throw error;
+    }
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <>
-      <Text>Header</Text>
-      <Slot />
-      <Text>Footer</Text>
-    </>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 };
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Page() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-red-500 text-5xl">Video platform</Text>
-      <Text className="text-yellow-500">This is the first page of your app.</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="font-pblack text-3xl">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="home" className="text-blue-500">
+        Go to Home
+      </Link>
     </View>
   );
 }
